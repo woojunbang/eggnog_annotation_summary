7cc256ec41c9b268d1321d1f40bded9108f69e7d
woojunbang <105182073+woojunbang@users.noreply.github.com>
2023-07-26 15:36:13 +0900

    Update eggnog_count_genes.py

--- eggnog_count_genes.py
+++ eggnog_count_genes.py
@@ -2,28 +2,31 @@ import argparse
 import pandas as pd
 
 def count_non_empty_genes(excel_file):
-    # 엑셀 파일 읽기
+    # load_file
     df = pd.read_excel(excel_file)
 
-    # 각 열에서 '-'이 아닌 유전자 개수 세기
+    # find
+    df['KEGG_Pathway'] = df['KEGG_Pathway'].fillna('')
+
+    # '-' except, count
     non_empty_GO = df[df['GOs'] != '-']['query'].nunique()
     non_empty_EC = df[df['EC'] != '-']['query'].nunique()
     non_empty_KEGG_ko = df[df['KEGG_ko'] != '-']['query'].nunique()
-    non_empty_KEGG_Pathway = df[df['KEGG_Pathway'] != '-']['query'].nunique()
+    non_empty_KEGG_Pathway = df[df['KEGG_Pathway'].apply(lambda x: x.strip() != '-')]['query'].nunique()
 
-    # 결과 출력
+    # Output display
     print("Non-empty genes in GOs column:", non_empty_GO)
     print("Non-empty genes in EC column:", non_empty_EC)
     print("Non-empty genes in KEGG_ko column:", non_empty_KEGG_ko)
     print("Non-empty genes in KEGG_Pathway column:", non_empty_KEGG_Pathway)
 
 if __name__ == "__main__":
-    # 커맨드 라인 인자 파서 생성
+    # parser
     parser = argparse.ArgumentParser(description='Count non-empty genes in various columns of an Excel file')
     parser.add_argument('input_file', type=str, help='Path to the input Excel file')
 
-    # 커맨드 라인 인자 파싱
+    # commandline parser
     args = parser.parse_args()
 
-    # 인자로 입력한 파일명으로 함수 호출
+    # function_display
     count_non_empty_genes(args.input_file)
